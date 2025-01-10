fa35590574fc1f50077552514f3e226e27688901
Soham Kapur <79128956+SKpro-glitch@users.noreply.github.com>
2025-01-10 14:58:39 +0530

    Update Prog_Count.v

--- Prog_Count.v
+++ Prog_Count.v
@@ -1,20 +1,19 @@
-module Prog_Count(clk,Reset,Jump,JumpTo,Address,Previous);
-    input clk, Reset, Jump;
-    input [9:0] JumpTo;
-    output [9:0]Address, Previous;
-    reg [9:0]Address, Previous;
-    
+module Prog_Count(
+    input clk, Reset, Jump,
+    input [9:0] JumpTo,
+    output reg [9:0] Address, Previous,
+    );
+        
     initial Address = 10'd0;
     
-    always @(posedge clk)
-    begin
-        Previous = Address;
+    always @(posedge clk) begin
+        Previous = Address; //Save the address of the previous instruction
         
         if(Reset)
             Address = 10'd0;
-        else if(Jump)
+        else if(Jump) //If branch is taken, jump to target address
             Address = JumpTo;
-        else
+        else //Else go to next address
             Address = Address + 10'd1;
     end
     
