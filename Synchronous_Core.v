a2518691e513efba3bfbb469f4f06034ff1738d3
Soham Kapur <79128956+SKpro-glitch@users.noreply.github.com>
2025-01-18 16:35:56 +0530

    Updated intermediate wires and registers in Synchronous_Core

--- Synchronous_Core.v
+++ Synchronous_Core.v
@@ -20,71 +20,80 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module Synchronous_Core(clk, Reset, out, file);
-    input clk, Reset;
-    output [31:0] out, file;
+module Synchronous_Core(
+    input clk, Reset,
+    
+    output [31:0] out, file
+    );
 //////////////////////////////////////////////////////////////////////////////////
 // DECLARATIONS
 
     //Fetch Unit declarations
-    wire Branch; //Branch status
-    wire [9:0] TargetAddress; //Target instruction address to jump to 
-    wire [31:0] Instruction; //Fetched instruction
-    wire [9:0] Add; //Current address of fetched instruction
+    wire Branch;
+    wire [9:0] TargetAddress;
+    wire [31:0] Instruction;
+    wire [9:0] Add;
     
     //Decode Unit declarations
-    wire [5:0] type; //Instruction type - R, I, S, B, U, J
-    wire [3:0] opcode; //Opcode to select type of instruction
-    wire [6:0] alu_opcode; //Opcode select operation of ALU
-    wire [4:0] rs0, rs1, rdt; //First source operand, Second source operand, Destination address of result
-    wire [2:0] funct3; //3-bit function
-    wire [6:0] funct7; //7-bit function
-    wire [19:0] imm; //Immediate value
-    
-    wire r, i, s, b, u, j; 
-    assign {r, i, s, b, u, j} = type; 
-    
+    wire valid;
+    wire [0:5] type;
+    wire [3:0] opcode; 
+    wire [6:0] alu_opcode;
+    wire [4:0] rs0, rs1, rdt;
+    wire [2:0] funct3;
+    wire [6:0] funct7;
+    wire [11:0] imm;
+        
     //Execute Unit declarations
     wire [31:0] op0, op1;
-    wire [31:0] result;
-    
+    wire s, i;
+    wire [4:0] write_address;
+    wire [9:0] mem_address;
+    wire [31:0] result;    
     assign out = result;
+    
+    //Memory Access and Writeback Unit declarations
+    wire [31:0] write_data;
+    reg [4:0] reg_file_address;
+    reg [31:0] alu_result;
+    reg type_s, type_i;
 //////////////////////////////////////////////////////////////////////////////////
-// FUNCTIONAL UNITS
+// SUPPLEMENTARY COMPONENTS
 
-    //RAM
-    wire write_enable;
+    //Instruction RAM
+    wire instruction_write;
     wire [31:0] data_in;
     wire [31:0] data_out;
-    reg [9:0] Ram_add;
-
-    //Instruction Memory
+    reg [9:0] instruction_address;
+        
     RAM i_ram(
-    .write_enable(write_enable),
-    .address(Ram_add),
+    .write_enable(instruction_enable),
+    .address(instruction_address),
     .data_in(data_in),
     .data_out(data_out)
     );
-
-    //If branch is present, fetch target address, else fetch next address
+    
     always @ * begin
         if(|Branch) 
-            Ram_add = TargetAddress;
+            instruction_address = TargetAddress;
         else 
-            Ram_add = Add;
+            instruction_address = Add;
     end
     
     //Register File
-    reg [31:0] Reg_File [0:15]; //16 registers of 32-bits each
-    assign op0 = rs0[4] ? Reg_File[rs0] : {28'b0, rs0[3:0]}; //Get first operand from Register File
-    assign op1 = rs1[4] ? Reg_File[rs1] : {28'b0, rs1[3:0]}; //Get second operand from Register File
-    wire [31:0] write_data; //Data to be written to the Reg File
-    assign file = Reg_File[0]; //The address zero of Reg File is given as output
-
-    //Data Memory
+    reg [31:0] Reg_File [0:15];
+    
+    assign op0 = rs0[4] ? Reg_File[rs0] : {28'b0, rs0[3:0]};
+    assign op1 = rs1[4] ? Reg_File[rs1] : {28'b0, rs1[3:0]};
+    
+    assign file = Reg_File[0];
+    
+    reg [9:0] data_address=0;
+    
+    //Data RAM
     RAM d_ram(
-    .write_enable(s),
-    .address(mem_address),
+    .write_enable(type_s),
+    .address(data_address),
     .data_in(store_data),
     .data_out(write_data)
     );
@@ -92,17 +101,22 @@ module Synchronous_Core(clk, Reset, out, file);
 //////////////////////////////////////////////////////////////////////////////////
     //PIPELINE BEGINS
     
+    // Stage 1
     Fetch fu(.clk(clk),
     .Reset(Reset),
     .Branch(Branch),
     .TargetAddress(TargetAddress),
+    .plus32(data_out),
+    
     .Instruction(Instruction),
-    .Add(Add),
-    .plus32(data_out)
+    .Add(Add)
     );
     
+    // Stage 2
     Decode du(.clk(clk),
     .instruction(Instruction), 
+    
+    .valid(valid),
     .type(type),
     .opcode(opcode), .alu_opcode(alu_opcode),
     .rs0(rs0), .rs1(rs1), .rdt(rdt),
@@ -110,26 +124,35 @@ module Synchronous_Core(clk, Reset, out, file);
     .imm(imm)
     );
     
+    // Stage 3
     Execute eu(.clk(clk),
+    .valid(valid),
     .type(type),
     .opcode(opcode), .alu_opcode(alu_opcode),
-    .op0(op0), .op1(op1),
+    .op0(op0), .op1(op1), .rdt(rdt),
     .funct3(funct3), .funct7(funct7),
     .imm(imm), .address(Add),
+    
     .targetAddress(TargetAddress),
+    .write_address(write_address),
     .result(result),
+    .s(s), .i(i),
     .branch(Branch),
     .mem_address(mem_address), 
     .store_data(store_data)
     );
     
-//////////////////////////////////////////////////////////////////////////////////
-    //  MEMORY ACCESS AND WRITE BACK LOGIC
-    
+    // Stage 4
+    //Memory Access and Write-Back logic
     always @ (posedge clk) begin
         //Writing to Register File / Store operation
-        if(s) Reg_File[rdt] <= write_data;
-        else if(i) Reg_File[rdt] <= result;        
+        reg_file_address <= write_address;
+        data_address <= mem_address;
+        alu_result <= result;
+        {type_s, type_i} <= {s, i};
+        #5;
+        if(type_s) Reg_File[reg_file_address] <= write_data;
+        else if(type_i) Reg_File[reg_file_address] <= alu_result;        
     end
     
 endmodule
