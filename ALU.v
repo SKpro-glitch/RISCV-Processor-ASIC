a53885141e2a28400d710d07ebbce532e609b9d4
Soham Kapur <79128956+SKpro-glitch@users.noreply.github.com>
2025-01-18 16:28:44 +0530

    Added Power Gating to ALU

--- ALU.v
+++ ALU.v
@@ -1,32 +1,54 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10.09.2024 15:27:57
+// Design Name: 
+// Module Name: ALU
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
 module ALU(
-    input clk, i,
+    input en, i,
     input [31:0] op0, op1,
     input [3:0] opcode,
     
     output reg [31:0] result  // ALU result
     );
-
-    reg signed [31:0] s_op0, s_op1; //Signed equivalents of the operands
-
-    always @ (posedge clk) begin
-        if(i) begin 
+    
+    reg signed [31:0] s_op0, s_op1;
+    
+    always @ (posedge en) begin
+        if(i) begin
             s_op0 = op0; 
             s_op1 = op1;
-            
-            case (opcode) // Legal ALU instructions
-                4'b0000  : result = op0 + op1 ; //Addition
-                4'b0001  : result = op0 - op1 ; //Subtraction
-                4'b0100  : result = {{63{1'b0}}, (s_op0 < s_op1)} ; // Signed less than
-                4'b0110  : result = {{63{1'b0}}, (op0 < op1)} ; // Unsigned less than
-                4'b1000  : result = op0 ^ op1 ; // Bitwise XOR
-                4'b1100  : result = op0 | op1 ; // Bitwise OR
-                4'b1110  : result = op0 & op1 ; // Bitwise AND
-                4'b0010  : result = op0 << op1[4:0] ; //Left shift
-                4'b1010  : result = op0 >> op1[4:0] ; //Right shift
-                4'b1011  : result = s_op0 >>> op1[4:0] ; //Signed right shift
-                default  : result = 0 ;
+            case (opcode)
+              // Legal ALU instructions
+              4'b0000  : result = op0 + op1 ; //Addition
+              4'b0001  : result = op0 - op1 ; //Subtraction
+              4'b0100  : result = {{63{1'b0}}, (s_op0 < s_op1)} ; // Signed less than
+              4'b0110  : result = {{63{1'b0}}, (op0 < op1)} ; // Unsigned less than
+              4'b1000  : result = op0 ^ op1 ; // Bitwise XOR
+              4'b1100  : result = op0 | op1 ; // Bitwise OR
+              4'b1110  : result = op0 & op1 ; // Bitwise AND
+              4'b0010  : result = op0 << op1[4:0] ; //Left shift
+              4'b1010  : result = op0 >> op1[4:0] ; //Right shift
+              4'b1011  : result = s_op0 >>> op1[4:0] ; //Signed right shift
+              default  : result = 0 ;
            endcase
        end
     end
-
+    
 endmodule
