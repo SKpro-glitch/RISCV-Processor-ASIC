b0c60cbce4b7aec7bb5fb4c89095afdcfb85aaaa
Soham Kapur <79128956+SKpro-glitch@users.noreply.github.com>
2025-01-18 16:34:03 +0530

    Added instruction validity flag to Decode Unit

--- Decode.v
+++ Decode.v
@@ -19,11 +19,12 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
-//Decode Stage - Decodes the instructino to obtain its functional components
+
 module Decode(
     input clk,
     input [31:0] instruction,
     
+    output reg valid,
     output reg [0:5] type,
     output reg [3:0] alu_opcode,
     output reg [6:0] opcode,
@@ -35,20 +36,23 @@ module Decode(
         
     wire r, i, s, b, u, j; 
     assign {r, i, s, b, u, j} = type; 
+    reg [31:0] Instr;
     
     always @ (posedge clk) begin
+        Instr = instruction;
         //Extracting the opcode from the instruction
-        opcode = instruction[6:0];
+        opcode = Instr[6:0];
         
         //Obtaining the operand source and target addresses from the instruction
-        rdt = instruction[11:7];
-        rs0 = instruction[19:15];
-        rs1 = instruction[24:20];
+        rdt = Instr[11:7];
+        rs0 = Instr[19:15];
+        rs1 = Instr[24:20];
         
         //Getting the functions from the instruction
-        funct3 = instruction[14:12];
-        funct7 = instruction[31:25];
+        funct3 = Instr[14:12];
+        funct7 = Instr[31:25];
         
+        valid = 1'b1;
         //Deciding type of instruction based on opcode
         case(opcode)
             7'h33: type = 6'b100000;
@@ -57,6 +61,7 @@ module Decode(
             7'h63: type = 6'b000100;
             7'h37, 7'h17: type = 6'b000010;
             7'h6f: type = 6'b000001;
+            default: valid = 1'b0;
         endcase
         
         //Selecting ALU Opcode based on type of instruciton
@@ -64,11 +69,11 @@ module Decode(
         else if(u) alu_opcode = 0;
         
         //Deciding Immediate value based on type of instruction
-        if(i) imm[11:0] = instruction[31:20];
-        else if(s) imm[11:0] = {funct7, instruction[11:7]};
-        else if(b) imm[11:0] = {instruction[31], instruction[7], funct7[5:0], instruction[11:8]};
-        else if(u) imm[19:0] = instruction[31:12];
-        else if(j) imm[19:0] = {instruction[31], instruction[19:12], instruction[20], instruction[30:21]};
+        if(i) imm[11:0] = Instr[31:20];
+        else if(s) imm[11:0] = {funct7, Instr[11:7]};
+        else if(b) imm[11:0] = {Instr[31], Instr[7], funct7[5:0], Instr[11:8]};
+        else if(u) imm[19:0] = Instr[31:12];
+        else if(j) imm[19:0] = {Instr[31], Instr[19:12], Instr[20], Instr[30:21]};
     end
 
 endmodule
