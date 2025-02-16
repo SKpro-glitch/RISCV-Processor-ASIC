19b1a12b838aa9b32b9030cee2578f78bd59040e
Soham Kapur <79128956+SKpro-glitch@users.noreply.github.com>
2025-02-16 11:32:28 +0530

    Update Branch.v

--- Branch.v
+++ Branch.v
@@ -24,7 +24,7 @@ module Branch(
     input en, b, j,
     input [2:0] funct3,
     input [19:0] imm,
-    input [31:0] op0, op1,
+    input [31:0] op1, op2,
     input [9:0] address,
     
     output reg branch,
@@ -32,23 +32,23 @@ module Branch(
     );
     
     wire [9:0] target;
-    reg signed [31:0] s_op0, s_op1;
+    reg signed [31:0] s_op1, s_op2;
     
     always @ (posedge en) begin
         targetAddress <= address + imm;
-        s_op0 <= op0; s_op1 <= op1;
+        s_op1 <= op1; s_op2 <= op2;
         
         //Branch Logic
         if(j) branch = 1;
         else if(b) begin
             branch = 1;
             case(funct3)
-                3'b000: branch = (op0 == op1);
-                3'b001: branch = ~(op0 == op1);
-                3'b100: branch = (s_op0 < s_op1);
-                3'b101: branch = ~(s_op0 < s_op1);
-                3'b110: branch = (op0 < op1);
-                3'b111: branch = ~(op0 < op1);
+                3'b000: branch = (op1 == op2);
+                3'b001: branch = ~(op1 == op2);
+                3'b100: branch = (s_op1 < s_op2);
+                3'b101: branch = ~(s_op1 < s_op2);
+                3'b110: branch = (op1 < op2);
+                3'b111: branch = ~(op1 < op2);
                 default: branch = 0;
             endcase
         end
