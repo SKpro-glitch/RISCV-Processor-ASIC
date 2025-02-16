341314c564a94f9d5399559a58bc3ecda08308f5
Soham Kapur <79128956+SKpro-glitch@users.noreply.github.com>
2025-02-16 11:31:28 +0530

    Update Decode.v

--- Decode.v
+++ Decode.v
@@ -28,7 +28,7 @@ module Decode(
     output reg [0:5] type,
     output reg [3:0] alu_opcode,
     output reg [6:0] opcode,
-    output reg [4:0] rs0, rs1, rdt,
+    output reg [4:0] rs1, rs2, rdt,
     output reg [2:0] funct3,
     output reg [6:0] funct7,
     output reg [19:0] imm
@@ -45,35 +45,47 @@ module Decode(
         
         //Obtaining the operand source and target addresses from the instruction
         rdt = Instr[11:7];
-        rs0 = Instr[19:15];
-        rs1 = Instr[24:20];
+        rs1 = Instr[19:15];
+        rs2 = Instr[24:20];
         
         //Getting the functions from the instruction
         funct3 = Instr[14:12];
         funct7 = Instr[31:25];
         
         valid = 1'b1;
+        #1;
         //Deciding type of instruction based on opcode
         case(opcode)
-            7'h33: type = 6'b100000;
-            7'h67, 7'h03, 7'h13: type = 6'b010000;
-            7'h23: type = 6'b001000;
-            7'h63: type = 6'b000100;
-            7'h37, 7'h17: type = 6'b000010;
-            7'h6f: type = 6'b000001;
+            7'h33: begin
+                type = 6'b100000;
+                imm = funct7;
+            end
+            7'h67, 7'h03, 7'h13: begin
+                type = 6'b010000;
+                imm = {funct7, rs2};
+            end
+            7'h23: begin
+                type = 6'b001000;
+            end
+            7'h63:begin
+                type = 6'b000100;
+                imm = {Instr[31], Instr[7], funct7[5:0], Instr[11:8]};
+            end
+            7'h37, 7'h17: begin
+                type = 6'b000010;
+                imm = {funct7, rs2, rs1, funct3};
+            end
+            7'h6f: begin
+                type = 6'b000001;
+                imm = {Instr[31], Instr[19:12], Instr[20], Instr[30:21]};
+            end
             default: valid = 1'b0;
         endcase
-        
+        #1;
         //Selecting ALU Opcode based on type of instruciton
         if(r | i) alu_opcode = {funct3, funct7[6]};
         else if(u) alu_opcode = 0;
         
-        //Deciding Immediate value based on type of instruction
-        if(i) imm[11:0] = Instr[31:20];
-        else if(s) imm[11:0] = {funct7, Instr[11:7]};
-        else if(b) imm[11:0] = {Instr[31], Instr[7], funct7[5:0], Instr[11:8]};
-        else if(u) imm[19:0] = Instr[31:12];
-        else if(j) imm[19:0] = {Instr[31], Instr[19:12], Instr[20], Instr[30:21]};
     end
 
 endmodule
