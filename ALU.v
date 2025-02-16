c29692eeab259c054ae1add155dbc4c1862f25d8
Soham Kapur <79128956+SKpro-glitch@users.noreply.github.com>
2025-02-16 11:33:14 +0530

    Update ALU.v

--- ALU.v
+++ ALU.v
@@ -21,34 +21,36 @@
 
 
 module ALU(
-    input en, i,
-    input [31:0] op0, op1,
-    input [3:0] opcode,
+    input en, r, i,
+    input [31:0] op1, op2, imm,
+    input [3:0] alu_opcode,
     
     output reg [31:0] result  // ALU result
     );
     
-    reg signed [31:0] s_op0, s_op1;
+    reg signed [31:0] op3, s_op1, s_op3;
     
     always @ (posedge en) begin
-        if(i) begin
-            s_op0 = op0; 
-            s_op1 = op1;
-            case (opcode)
+            #1;
+            if(r) op3 = op2;
+            else op3 = imm;
+            
+            s_op1 = op1; s_op3 = op3;
+            
+            case (alu_opcode)
               // Legal ALU instructions
-              4'b0000  : result = op0 + op1 ; //Addition
-              4'b0001  : result = op0 - op1 ; //Subtraction
-              4'b0100  : result = {{63{1'b0}}, (s_op0 < s_op1)} ; // Signed less than
-              4'b0110  : result = {{63{1'b0}}, (op0 < op1)} ; // Unsigned less than
-              4'b1000  : result = op0 ^ op1 ; // Bitwise XOR
-              4'b1100  : result = op0 | op1 ; // Bitwise OR
-              4'b1110  : result = op0 & op1 ; // Bitwise AND
-              4'b0010  : result = op0 << op1[4:0] ; //Left shift
-              4'b1010  : result = op0 >> op1[4:0] ; //Right shift
-              4'b1011  : result = s_op0 >>> op1[4:0] ; //Signed right shift
+              4'b0000  : result = op1 + op3 ; //Addition
+              4'b0001  : result = op1 - op3 ; //Subtraction
+              4'b0100  : result = {{63{1'b0}}, (s_op1 < s_op3)} ; // Signed less than
+              4'b0110  : result = {{63{1'b0}}, (op1 < op3)} ; // Unsigned less than
+              4'b1000  : result = op1 ^ op3 ; // Bitwise XOR
+              4'b1100  : result = op1 | op3 ; // Bitwise OR
+              4'b1110  : result = op1 & op3 ; // Bitwise AND
+              4'b0010  : result = op1 << op3[4:0] ; //Left shift
+              4'b1010  : result = op1 >> op3[4:0] ; //Right shift
+              4'b1011  : result = s_op1 >>> op3[4:0] ; //Signed right shift
               default  : result = 0 ;
            endcase
-       end
     end
     
 endmodule
