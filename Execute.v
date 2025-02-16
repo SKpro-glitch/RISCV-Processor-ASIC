789e78c6a6104a7af15a43a6cf24e43a1c03efa3
Soham Kapur <79128956+SKpro-glitch@users.noreply.github.com>
2025-02-16 11:32:05 +0530

    Update Execute.v

--- Execute.v
+++ Execute.v
@@ -25,7 +25,7 @@ module Execute(
     input [0:5] type,
     input [3:0] alu_opcode,
     input [6:0] opcode,
-    input [31:0] op0, op1,
+    input [31:0] op1, op2,
     input [4:0] rdt,
     input [2:0] funct3,
     input [6:0] funct7,
@@ -33,10 +33,10 @@ module Execute(
     input [9:0] address,
     
     output reg [4:0] write_address,
-    output [31:0] result,
-    output s, i, branch,
+    output reg s, i, 
+    output branch,
     output [31:0] mem_address,
-    output [31:0] store_data,
+    output reg [31:0] store_data,
     output [9:0] targetAddress
     );
     
@@ -44,7 +44,7 @@ module Execute(
     reg [0:5] type_pipo;
     reg [3:0] alu_opcode_pipo;
     reg [6:0] opcode_pipo;
-    reg [31:0] op0_pipo, op1_pipo;
+    reg [31:0] op1_pipo, op2_pipo;
     reg [2:0] funct3_pipo;
     reg [6:0] funct7_pipo;
     reg [19:0] imm_pipo;
@@ -52,55 +52,71 @@ module Execute(
          
     reg en_alu=0, en_bu=0, en_lsu=0;
     
-    wire r, b, u, j; 
-    assign {r, i, s, b, u, j} = type_pipo; 
-
+    reg r, b, u, j; 
+    
+    wire [31:0] alu_result, lsu_out;
+    
     always @ (posedge clk) begin
-        valid_check <= valid;
-        #1;
-        if(valid) begin        
-            type_pipo <= type;
-            alu_opcode_pipo <= alu_opcode;
-            opcode_pipo <= opcode;
-            op0_pipo <= op0; op1_pipo <= op1;
-            funct3_pipo <= funct3;
-            funct7_pipo <= funct7;
-            imm_pipo <= imm;
-            address_pipo <= address;
-            write_address <= rdt;
+        valid_check = valid;
+        {en_alu, en_bu, en_lsu} = 3'b000;
+        if(valid_check) begin        
+            {r, i, s, b, u, j} = type;
+            alu_opcode_pipo = alu_opcode;
+            opcode_pipo = opcode;
+            op1_pipo = op1; op2_pipo = op2;
+            funct3_pipo = funct3;
+            funct7_pipo = funct7;
+            imm_pipo = imm;
+            address_pipo = address;
+            write_address = rdt;
             
-            if(i) {en_alu, en_bu, en_lsu} = 3'b100;
+            #1;
+            
+            if(r) {en_alu, en_bu, en_lsu} = 3'b100;
             else if(b | j) {en_alu, en_bu, en_lsu} = 3'b010;
             else if(s) {en_alu, en_bu, en_lsu} = 3'b001;
+            else if(i) begin
+                if(opcode==7'h13) {en_alu, en_bu, en_lsu} = 3'b100;
+                else {en_alu, en_bu, en_lsu} = 3'b001;
+            end
+            else {en_alu, en_bu, en_lsu} = 3'b000;
+
+            #2;
+            
+            if(en_alu) store_data = alu_result;
+            else if(en_lsu) store_data = lsu_out;
+            else if(en_bu) begin end 
+            else store_data = {{10{1'b0}}, imm_pipo};
         end
     end
-    
-    always @ (negedge clk) {en_alu, en_bu, en_lsu} = 3'b000;
-    
+        
     Branch bu(
     .en(en_bu),
     .b(b), .j(j),
     .funct3(funct3_pipo), .imm(imm_pipo),
-    .op0(op0_pipo), .op1(op1_pipo),
+    .op1(op1_pipo), .op2(op2_pipo),
     .address(address_pipo),
+    
     .branch(branch),
     .targetAddress(targetAddress));
     
     ALU alu(
     .en(en_alu),
-    .i(i),
-    .op0(op0_pipo), .op1(op1_pipo), 
-    .opcode(alu_opcode_pipo),
-    .result(result));
+    .r(r), .i(i),
+    .op1(op1_pipo), .op2(op2_pipo),
+    .imm(imm_pipo),
+    .alu_opcode(alu_opcode_pipo),
+    
+    .result(alu_result));
     
     Loadstore lsu(
     .en(en_lsu),
     .s(s), 
-    .opcode(opcode_pipo), 
     .funct3(funct3_pipo), 
     .imm(imm_pipo[11:0]), 
-    .op0(op0_pipo), .op1(op1_pipo), 
+    .op1(op1_pipo), .op2(op2_pipo), 
+    
     .mem_address(mem_address), 
-    .store_data(store_data));
+    .store_data(lsu_out));
 
 endmodule
